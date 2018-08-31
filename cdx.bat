4859b30d705fb11e79ba9edd5a4f722303497fd2
Gilles Arcas <gilles.arcas@gmail.com>
2018-08-31 23:39:16 +0200

    "you cannot run a Doskey macro from a batch file". No need to try a trick by using chdir instead of cd.

--- cdx.bat
+++ cdx.bat
@@ -3,9 +3,9 @@ rem install: doskey cd=<full path>/cdx.bat $*
 
 if "%~1"=="*" goto guiselect
 
-chdir /d "%~1"
+cd /d "%~1"
 py %~dpn0.py insert "%cd%"
 goto :eof
 
 :guiselect
-for /F "delims=*" %%1 in ('py %~dpn0.py select d:/') do chdir /d %%1
+for /F "delims=*" %%1 in ('py %~dpn0.py select d:/') do cd /d %%1
